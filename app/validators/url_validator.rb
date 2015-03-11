455ce03754eb881f630852be3d6198dc2825bd31
Tianyu Huang <tianhsky@gmail.com>
2015-03-11 06:02:04 +0000

    fix url regex

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,6 +1,6 @@
 class UrlValidator < ActiveModel::EachValidator
 	def validate_each(record, attribute, value)
-		unless value =~ /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+		unless value =~ /^(https?:\/\/)((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+([A-Za-z]{2,6})(\S*)$/
 			record.errors[attribute] << (options[:message] || "is not a valid url")
 		end
 	end
